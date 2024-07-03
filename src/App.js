86cc4e64383063261ea6f2f534eca883a8b2afb4
ismail khan <imismail885@gmail.com>
2024-07-03 22:19:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi, there we learn something new we, are 
+          <code> Learning Docker from scratch</code>
+          by there whatsaap. 
+          i was changed.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
